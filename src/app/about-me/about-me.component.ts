14d7385e8b24b33710cfc2140868643c7fe8c413
kaoser <kaosermail@gmail.com>
2022-05-01 22:28:23 +0200

    starting about me section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -4,22 +4,35 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   selector: 'app-about-me',
   template: `
     <div id="video-div">
-      <video id="video" height="100%" width="100%" autoplay muted loop preload #videoRef>
+      <img id="splashArt" src="assets/imgs/kaoser.png">
+      <img id="videoShape" src="./assets/imgs/VideoShape.png">
+      <p class="navbar-item is-size-4 is-family-arial">Rodrigo Alonso</p>
+    </div>
+      <div id="about-me">
+        <div class="me-text title is-family-arial is-size-3">
+          GAME DEVELOPER AND PLAYER
+          <hr>
+        </div>
+        <div class="me-text is-family-sans-serif has-text-left">
+            From Playmobil, Magic: The Gathering or Warhammer, I have always enjoyed playing games and creating them my own. It was already before even starting Computer Science BA that I knew I wanted to base my career on it.
+            <br><br>
+            When I finished my Master, I had already around 10 games built from scratch with Unity, Unreal Engine and Gamemaker. I learnt from trial and error about fundamentals of design, team collaboration and direction, a little 2D and 3D art, and software: my speciality. 
+            <br><br>
+            My skills cover from high level architecture to low level code details, having extensive experience in object-oriented design, network programming, web technologies, databases, multi-threading and asynchronous programming. 
+            <br><br>
+            My mission is to create the best entertainment possible for players by using this new form of art which I love and in which I will put all my passion, creativity and effort. This is why I also focus on reviewing the games I play, stream on Twitch to discuss about them and program new side projects every day.
+            <br><br>
+            On this web you will find my best projects along with my reviews. Enjoy!
+        </div>
+
+      <div id="photo">
+        <img src="assets/imgs/Rodrigo.png">
+      </div>
+
+      <!-- <video id="video" height="40%" width="40%" autoplay muted loop preload #videoRef>
           <source type="video/mp4" src="assets/videos/Oculus.mp4" >
           <source type="video/ogg" src="assets/videos/Oculus.ogg" >
-      </video>
-      <img id="videoShape" src="./assets/imgs/VideoShape.png">
-    </div>
-
-    <div class="card">
-      <div class="card-content">
-        <div class="content is-family-arial has-text-centered">
-            Game Developer and Player. <br><br>
-            I studied Computer Science and a Master in Game Technology and, most importantly, I have developed my own games from engines like Unity, Unreal, and other creative tools. <br><br>
-            My mission is to create the best entertainment possible for players by using this new form of art which I love. I will put all my passion, creativity and effort into this task. <br><br>
-            On this web you will find some of the games I have made. Most of their assets are self made. Enjoy!
-        </div>
-      </div>
+      </video> -->
     </div>
   `,
   styles: [
@@ -28,13 +41,13 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 export class AboutMeComponent implements OnInit {
   
-  ​@ViewChild('videoRef', { static: true }) videoRef!: ElementRef
+  // ​@ViewChild('videoRef', { static: true }) videoRef!: ElementRef
 
-  ​ngAfterViewInit(): void {
-    ​const media = this.videoRef.nativeElement
-    ​media.muted = true 
-    ​media.play() 
-  ​ } 
+  // ​ngAfterViewInit(): void {
+  //   ​const media = this.videoRef.nativeElement
+  //   ​media.muted = true
+  //   ​media.play() 
+  // ​ } 
 
   constructor() { }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,26 +4,26 @@ import { Component, OnInit } from '@angular/core';
     selector: 'app-header',
     template: `
     <nav class="navbar is-black is-fixed-top" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <figure class="image is-64x64">
-            <img class="navbar-item" src="assets/icons/Kaoser.png">
-        </figure>
-        <p class="navbar-item is-size-4 is-family-arial">Rodrigo Alonso</p>
-    </div>
-    <div class="navbar-end">
-        <figure class="image is-64x64">
-            <img class="navbar-item" src="assets/icons//GitHub.png">
-        </figure>
-        <figure class="image is-64x64">
-            <img class="navbar-item" src="assets/icons//Linkedin.png">
-        </figure>
-        <figure class="image is-64x64">
-            <img class="navbar-item" src="assets/icons//Youtube.png">
-        </figure>
-        <figure class="image is-64x64">
-            <img class="navbar-item" src="assets/icons//Twitch.png">
-        </figure>
-    </div>
+        <div class="navbar-brand">
+            <div class="navbar-item image">
+                <img src="assets/icons/Kaoser.png">
+            </div>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarSocials">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div id="navbarSocials" class="navbar-menu">
+            <div class="navbar-end">
+                <div class="field is-grouped">
+                    <img class="navbar-item image" src="assets/icons/GitHub.png">
+                    <img class="navbar-item image" src="assets/icons/Linkedin.png">
+                    <img class="navbar-item image" src="assets/icons/Youtube.png">
+                    <img class="navbar-item image" src="assets/icons/Twitch.png">
+                </div>
+            </div>
+        </div>
     </nav>
   `,
     styles: [

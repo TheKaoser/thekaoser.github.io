5af665fcf00e2b8a2c2f594be0782211ba52ca2f
kaoser <kaosermail@gmail.com>
2022-05-15 00:05:18 +0200

    burger menu finish

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -8,18 +8,18 @@ import { Component, OnInit, ViewChild } from '@angular/core';
             <a class="navbar-item ml-2 p-4" routerLink=""><img src="assets/icons/Kaoser.png"></a>
             <a class="navbar-item mx-4 is-size-6 is-italic" routerLink="projects">PROJECTS</a>
             <a class="navbar-item mx-4 is-size-6 is-italic" routerLink="reviews">REVIEWS</a>
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarSocials">
+            <a (click)="toggleNavbar()" #navBurger role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarSocials">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
             </a>
         </div>
-        <div id="navbarSocials" class="navbar-menu">
-            <div class="navbar-end">
-                <a class="navbar-item p-4 mr-3" href="https://github.com/TheKaoser" target="_blank"><img src="assets/icons/GitHub.png"></a>
-                <a class="navbar-item p-4 mr-3" href="https://www.twitch.tv/thekaoser" target="_blank"><img src="assets/icons/Twitch.png"></a>
+        <div id="navbarSocials" class="navbar-menu has-background-black" #navMenu>
+            <div class="navbar-end is-right">
+                <a class="navbar-item p-4 mr-3 is-pulled-right has-background-black" href="https://github.com/TheKaoser" target="_blank"><img src="assets/icons/GitHub.png"></a>
+                <a class="navbar-item p-4 mr-3 is-pulled-right has-background-black" href="https://www.twitch.tv/thekaoser" target="_blank"><img src="assets/icons/Twitch.png"></a>
                 <!-- <a class="navbar-item p-4 mr-3" href="https://www.linkedin.com/in/rodrigo-alonso-garcía-amorena/" target="_blank"><img src="assets/icons/Linkedin.png"></a> -->
-                <a class="navbar-item p-4 mr-3" href="https://www.youtube.com/channel/UCVJ3aNNjDRFC-OzKfaAbTrQ" target="_blank"><img src="assets/icons/Youtube.png"></a>
+                <a class="navbar-item p-4 mr-3 is-pulled-right has-background-black" href="https://www.youtube.com/channel/UCVJ3aNNjDRFC-OzKfaAbTrQ" target="_blank"><img src="assets/icons/Youtube.png"></a>
             </div>
         </div>
     </nav>
@@ -27,12 +27,18 @@ import { Component, OnInit, ViewChild } from '@angular/core';
     styles: [
     ]
 })
+
 export class HeaderComponent implements OnInit {
+    @ViewChild('navBurger') navBurger!: ElementRef;
+    @ViewChild('navMenu') navMenu!: ElementRef;
+
+    toggleNavbar() {
+        this.navBurger.nativeElement.classList.toggle('is-active');
+        this.navMenu.nativeElement.classList.toggle('is-active');
+    }
 
     constructor() { }
 
-    ngOnInit(): void {
-    }
-
+    ngOnInit(): void { }
 }
 

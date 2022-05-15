7c9e160e070e3973d7591d9207698fb708e06b96
kaoser <kaosermail@gmail.com>
2022-05-15 20:59:30 +0200

    projects' video

--- src/app/projects/about-projects/about-projects.component.ts
+++ src/app/projects/about-projects/about-projects.component.ts
@@ -3,12 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-about-projects',
 	template: `
-    <video id="video" height="100%" width="100%" autoplay muted loop preload #videoRef>
-		<source type="video/mp4" src="assets/videos/Oculus.mp4">
-		<source type="video/ogg" src="assets/videos/Oculus.ogg">
-	</video>
+	<div class="cover-div">
+		<video id="splashArt" height="100%" width="100%" autoplay muted loop preload #videoRef>
+			<source type="video/mp4" src="assets/videos/Projects.mp4">
+		</video>
+		<img id="videoShape" src="./assets/imgs/VideoShape.png">
+	</div>
+	<div id="projects">
+		<p>Hola</p>
+	</div>
   `,
 	styles: [
+		'#splashArt { position:absolute; }',
+		'#videoShape { position:absolute; }',
+		'#projects { position: relative; margin-top: 100px; }',
 	]
 })
 export class AboutProjectsComponent implements OnInit {
